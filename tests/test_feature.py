5423732ecf2a4c83cb3d58076bc5424656244e9a
Den <denismednov@gmail.com>
2024-10-25 22:50:47 +0300

    sdf

--- tests/test_feature.py
+++ tests/test_feature.py
@@ -3,13 +3,13 @@ import os
 from src.content_manager.client import experiment_feature
 
 
-
+def test_retrieve_os_variable():
+    value = os.environ["GOOGLE_API_KEY"]
+    assert value
 
 def test_if_it_works():
     api_key = os.environ["GOOGLE_API_KEY"]
-    if api_key:
-        print("TRUEEEEEE")
-
+    
     response = experiment_feature(api_key)
 
     assert response
