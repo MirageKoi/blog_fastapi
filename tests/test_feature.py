c50526ad182ada2450e8e6d86e9953ac196a4bac
Den <denismednov@gmail.com>
2024-10-25 22:42:33 +0300

    123

--- tests/test_feature.py
+++ tests/test_feature.py
@@ -1,11 +1,16 @@
+import os
+
 from src.content_manager.client import experiment_feature
 
 
 
 
 def test_if_it_works():
+    api_key = os.environ["GOOGLE_API_KEY"]
+    if api_key:
+        print("TRUEEEEEE")
 
-    response = experiment_feature()
+    response = experiment_feature(api_key)
 
     assert response
 
