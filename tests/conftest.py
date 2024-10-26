3e77c3da1bfffe9c0f963fe7c281dcdf4931d0cb
Den <denismednov@gmail.com>
2024-10-26 11:57:51 +0300

    Upd conftest file. Add new fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,16 @@
 import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from src.adapters.database import Base
+from fastapi.testclient import TestClient
+from sqlalchemy import StaticPool, create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from src.adapters.database import Base, get_db
+from src.auth.routers import get_current_user
+from src.auth.schemas import DecodedToken
+from src.main import app
+
+
+def override_get_current_user():
+    return DecodedToken(id=1, username="testuser", email="testemail")
 
 
 @pytest.fixture
@@ -15,3 +24,26 @@ def in_memory_db():
 def session(in_memory_db):
     with sessionmaker(bind=in_memory_db)() as session:
         yield session
+
+
+@pytest.fixture
+def api_session():
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    Base.metadata.create_all(engine)
+    with sessionmaker(engine, autocommit=False, autoflush=False)() as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(api_session: Session):
+    def get_session_override():
+        return api_session
+
+    app.dependency_overrides[get_db] = get_session_override
+    app.dependency_overrides[get_current_user] = override_get_current_user
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
