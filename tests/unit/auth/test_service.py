ca61e3564be472bd93a4534e3201eebcb2018bb4
Den <denismednov@gmail.com>
2024-10-26 12:16:39 +0300

    Add tests for authorization

--- tests/unit/auth/test_service.py
+++ tests/unit/auth/test_service.py
@@ -1,12 +1,13 @@
 from datetime import timedelta
-from fastapi import HTTPException
+
 import pytest
-from src.auth.routers import create_access_token, get_current_user
+from fastapi import HTTPException
 from jose import jwt
 
+from src.auth.config import get_auth_settings
+from src.auth.routers import create_access_token, get_current_user
 
-SECRET_KEY = "197b2c37c391bed93fe80344fe73b806947a65e36206e05a1a23c2fa12702fe3"
-ALGORITHM = "HS256"
+settings = get_auth_settings()
 
 
 def test_create_access_token():
@@ -18,7 +19,10 @@ def test_create_access_token():
     token = create_access_token(username, user_id, email, expires_delta)
 
     decoded_token = jwt.decode(
-        token, SECRET_KEY, algorithms=[ALGORITHM], options={"verify_signature": False}
+        token,
+        settings.secret_key,
+        algorithms=[settings.algorithm],
+        options={"verify_signature": False},
     )
 
     assert decoded_token["sub"] == username
@@ -28,15 +32,19 @@ def test_create_access_token():
 
 async def test_get_current_user_valid_token():
     encode = {"sub": "testuser", "id": 1, "email": "testemail@gmail.com"}
-    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    token = jwt.encode(encode, settings.secret_key, algorithm=settings.algorithm)
 
     user = await get_current_user(token=token)
-    assert user.model_dump() == {"username": "testuser", "id": 1, "email": "testemail@gmail.com"}
+    assert user.model_dump() == {
+        "username": "testuser",
+        "id": 1,
+        "email": "testemail@gmail.com",
+    }
 
 
 async def test_get_current_user_missing_payload():
     encode = {"role": "user"}
-    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    token = jwt.encode(encode, settings.secret_key, algorithm=settings.algorithm)
 
     with pytest.raises(HTTPException) as excinfo:
         await get_current_user(token=token)
