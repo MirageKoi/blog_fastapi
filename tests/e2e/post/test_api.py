d0fead807dfa1c7a96880e560626520acd5b71e1
Den <denismednov@gmail.com>
2024-10-26 11:52:14 +0300

    Add e2e post tests

--- tests/e2e/post/test_api.py
+++ tests/e2e/post/test_api.py
@@ -1,39 +1,4 @@
 import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import StaticPool, create_engine
-from sqlalchemy.orm import Session, sessionmaker
-
-from src.adapters.database import Base, get_db
-from src.auth.routers import get_current_user
-from src.auth.schemas import DecodedToken
-from src.main import app
-
-
-def override_get_current_user():
-    return DecodedToken(id=1, username="testuser", email="testemail")
-
-
-@pytest.fixture(name="sessionv2")
-def session_fixture():
-    engine = create_engine(
-        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
-    )
-    Base.metadata.create_all(engine)
-    with sessionmaker(engine, autocommit=False, autoflush=False)() as session:
-        yield session
-
-
-@pytest.fixture(name="client")
-def client_fixture(sessionv2: Session):
-    def get_session_override():
-        return sessionv2
-
-    app.dependency_overrides[get_db] = get_session_override
-    app.dependency_overrides[get_current_user] = override_get_current_user
-
-    client = TestClient(app)
-    yield client
-    app.dependency_overrides.clear()
 
 
 @pytest.mark.asyncio
