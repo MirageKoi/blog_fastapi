f1dfdba80d55bdb64cafab0c7cdc686725e0c299
Den <denismednov@gmail.com>
2024-11-06 12:37:54 +0200

    Upd repository implementation. Return schemas models

--- src/posts/repository.py
+++ src/posts/repository.py
@@ -1,49 +1,64 @@
-from typing import Any, Sequence
+from typing import Any, Protocol
 
-from sqlalchemy import select
+from sqlalchemy import delete, select, update
 from sqlalchemy.orm import Session
 
+from src.posts.schemas import CommentResponseDB, PostResponseDB
+
 from .models import Comment, Post
+
+
+class IPostRepository(Protocol):
+    async def get_post_all(self) -> list[PostResponseDB | None]: ...
+
+    async def get_post_by_id(self, id: int) -> PostResponseDB | None: ...
+
+    async def create_post(self, values: dict[str, Any]) -> PostResponseDB: ...
+
+    async def update_post(self, id: int, values: dict[str, Any]) -> PostResponseDB: ...
+
+    async def delete_post(self, id: int) -> None: ...
+
+    async def create_comment(self, values: dict[str, Any]) -> CommentResponseDB: ...
 
 
 class PostRepository:
     def __init__(self, session: Session) -> None:
         self.session = session
 
-    async def get_post_all(self) -> Sequence[Post]:
+    async def get_post_all(self) -> list[PostResponseDB]:
         stmt = select(Post)
         res = self.session.scalars(stmt).all()
-        return res
+        return [PostResponseDB(**entity.__dict__) for entity in res]
 
-    async def get_post_by_id(self, pk: int) -> Post | None:
-        res = self.session.get(Post, pk)
-        return res
+    async def get_post_by_id(self, id: int) -> PostResponseDB | None:
+        res = self.session.get(Post, id)
+        if res:
+            return PostResponseDB(**res.__dict__)
+        else:
+            return None
 
-    async def create_post(self, values: dict[str, Any]) -> Post:
+    async def create_post(self, values: dict[str, Any]) -> PostResponseDB:
         instance = Post(**values)
         self.session.add(instance)
         self.session.commit()
         self.session.refresh(instance)
 
-        return instance
+        return PostResponseDB(**instance.__dict__)
 
-    async def update_post(self, db_post: Post, values: dict[str, Any]) -> Post:
-        instance = db_post
-        for k, v in values.items():
-            setattr(instance, k, v)
+    async def update_post(self, id: int, values: dict[str, Any]) -> PostResponseDB:
+        stmt = update(Post).where(Post.id == id).values(**values).returning(Post)
+        instance = self.session.scalar(stmt)
 
-        self.session.commit()
+        return PostResponseDB.model_validate(instance.__dict__)
 
-        return instance
+    async def delete_post(self, id: int) -> None:
+        self.session.execute(delete(Post).where(Post.id == id))
 
-    async def delete_post(self, db_post: Post) -> None:
-        self.session.delete(db_post)
-        self.session.commit()
-
-    async def create_comment(self, values: dict[str, Any]):
+    async def create_comment(self, values: dict[str, Any]) -> CommentResponseDB:
         instance = Comment(**values)
         self.session.add(instance)
         self.session.commit()
         self.session.refresh(instance)
 
-        return instance
+        return CommentResponseDB.model_validate(instance.__dict__)
